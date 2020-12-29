5907931d80bd2bfeb01b1fd0bed7fda9d7d260d5
BOYI <as20757738@gmail.com>
2020-12-29 11:46:45 +0800

    update sheep animation

--- GObject/Sheep.py
+++ GObject/Sheep.py
@@ -3,51 +3,79 @@ import pygame as pg
 from GObject.BaseGObject import BaseGObject
 from Utils.Utils import clamp
 
+
 class Sheep(BaseGObject):
     def __init__(self, x, y):
         animated = [
             {
                 'path': 'media/sheep/sheep_0.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_1.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_2.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_3.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_4.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_5.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_6.png',
-                'duration': 200
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_7.png',
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_8.png',
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_9.png',
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_8.png',
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_7.png',
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_6.png',
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_5.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_4.png',
-                'duration': 200
+                'duration': 100
             },
             {
                 'path': 'media/sheep/sheep_3.png',
-                'duration': 200
-            }
+                'duration': 100
+            },
+            {
+                'path': 'media/sheep/sheep_2.png',
+                'duration': 100
+            },
         ]
-
 
         super(Sheep, self).__init__(animated, 0.125, x, y)
         self.setMaxSpeed(10)
